e65c9e3b2072c931decde9065baa95c2ca06aded
insiraseunome <e8716394@gmail.com>
2026-05-17 11:00:32 -0300

    pequenas implementações feitas. início do desenvolvimento

---
+++ styles/global-styles.tsx
@@ -0,0 +1,31 @@
+//import { StyleSheet } from "react-native"
+
+/*const styles = StyleSheet.create({
+  
+})
+
+export const fonts = {
+    fontSize: 12,
+
+  }
+
+export const radius = {
+    sm: 13,
+    md: 14,
+    lg: 18
+  }
+
+export const spacing = {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 18
+  }
+
+export const colors = {
+    oxfordNavy: '#003366',
+    pearlAqua: '#A4D1CA',
+    inkBlack: '#11181C',
+    white: '#FFFFFF',
+    coolSteel: '#9BA1A6'
+  }*/
